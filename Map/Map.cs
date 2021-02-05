b7c36377a59f7ad226c59e209986eb47c41a0c77
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-02-06 10:27:00 +1300

    First commit

---
+++ Map/Map.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RogueLike.Entities;
+
+namespace RogueLike.Map
+{
+    class Map
+    {
+        private MapElement[,] Elements { get; set; }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RogueLike.Entities;
+using RogueLike.Items;
+
+namespace RogueLike
+{
+    class Program
+    {
+        static void Main()
+        {
+            Inventory inv = new Inventory();
+
+            inv.AddItem(new Item("as"));
+            Console.ReadKey();
+        }
+    }
+}
