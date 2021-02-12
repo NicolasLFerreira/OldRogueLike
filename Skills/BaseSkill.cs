8a323e3366680c7a2b55e10228e4cfb5babe1226
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-02-13 11:59:02 +1300

    Revamped skill system

---
+++ Skills/BaseSkill.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RogueLike.Elements;
+
+namespace RogueLike.Skills
+{
+    class BaseSkill
+    {
+        public BaseSkill(MapEntity user, int cost) // Sets the caster and cost
+        {
+            User = user;
+            Cost = cost;
+        }
+
+        public MapEntity User { get; set; }
+        public int Cost { get; set; }
+
+        virtual public void Cast(MapEntity target)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Skills/Offensive/BasicAttack.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RogueLike.Elements;
+
+namespace RogueLike.Skills.Offensive
+{
+    class BasicAttack : BaseSkill
+    {
+        public BasicAttack(MapEntity user, int cost) : base(user, cost) { }
+
+        public MapEntity User { get; set; }
+        public int Cost { get; set; }
+
+        public override void Cast(MapEntity target) => target.ReceiveDamage(User.Atk - target.Defense);
+    }
+}
