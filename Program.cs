e19d7c352935e6824bcd9e75f6ad13c873f0b9b7
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-02-13 11:59:24 +1300

    Code cleanup

--- Program.cs
+++ Program.cs
@@ -5,8 +5,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using RogueLike.Entities;
+using RogueLike.Elements;
 using RogueLike.Items;
+using RogueLike.Skills;
 
 namespace RogueLike
 {
