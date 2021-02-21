9b1f231a03e9180e5b96dfac2299a0022345ea70
Nicolas Ferreira <nicolas.l.ferreira@gmail.com>
2021-02-22 11:29:30 +1300

    Created the method for printing inventory content

---
+++ Graphics/PrintInventory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using RogueLike.Items;
+
+namespace RogueLike.Graphics
+{
+    class PrintInventory
+    {
+        static public void PrintContent(Inventory targetInventory)
+        {
+            Console.WriteLine($"Empty slots: {targetInventory.Cap - targetInventory.Count}");
+            Console.WriteLine($"Used slots: {targetInventory.Count}\n");
+
+            for (int i = 0; i < targetInventory.Cap; i++)
+            {
+                if (targetInventory[i].Type == -1) Console.WriteLine($"- {i + 1}: EMPTY");
+                else Console.WriteLine($"- {i + 1}: {targetInventory[i].Name} : {targetInventory[i].Icon}");
+            }
+        }
+    }
+}
